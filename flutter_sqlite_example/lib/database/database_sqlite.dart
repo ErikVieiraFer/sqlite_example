6150c6fbed113bdb8ea0b762d91409d5b4811fa4
ErikVieiraFer <erik.vieiradev@hotmail.com>
2025-06-17 21:49:50 -0300

    Como atualizar e voltar versao de banco de dados

--- flutter_sqlite_example/lib/database/database_sqlite.dart
+++ flutter_sqlite_example/lib/database/database_sqlite.dart
@@ -9,7 +9,11 @@ class DatabaseSqLite {
 
     await openDatabase(
       databaseFinalPath,
-      version: 1,
+      version: 2,
+      onConfigure: (db) async {
+        print('onConfigure sendo chamado');
+        await db.execute('PRAGMA foreign_keys = ON');
+      },
       // Chamado somente no momento de criação do banco de dados
       // primeira vez que carrega o aplicativo
       onCreate: (Database db, int version) {
@@ -22,16 +26,62 @@ class DatabaseSqLite {
           nome varchar(200)
         )
       ''');
+        batch.execute('''
+        create table product(
+          id Integer primary key autoincrement
+          nome varchar(200)
+        )
+      ''');
+        //   batch.execute('''
+        //   create table category(
+        //     id Integer primary key autoincrement
+        //     nome varchar(200)
+        //   )
+        // ''');
       },
 
       //Será chamado sempre que houver uma alteração no version incremental (1 -> 2)
       onUpgrade: (Database db, int oldVersion, int newVersion) {
         print('onUpgrade Chamado');
+        final batch = db.batch();
+        if (oldVersion == 1) {
+          batch.execute('''
+        create table product(
+          id Integer primary key autoincrement
+          nome varchar(200)
+        )
+      ''');
+
+          // batch.execute('''
+          //   create table category(
+          //     id Integer primary key autoincrement
+          //     nome varchar(200)
+          //   )
+          // ''');
+        }
+
+        //   if (oldVersion == 2) {
+        //     batch.execute('''
+        //   create table category(
+        //     id Integer primary key autoincrement
+        //     nome varchar(200)
+        //   )
+        // ''');
+        //   }
+
+        batch.commit();
       },
 
       //Será chamado sempre que houver uma alteração no version decremental (2 -> 1)
       onDowngrade: (Database db, int oldVersion, int newVersion) {
         print('onDowngrade Chamado');
+        final batch = db.batch();
+        if (oldVersion == 3) {
+          batch.execute('''
+          drop table category;          
+          ''');
+        }
+        batch.commit();
       },
     );
   }
